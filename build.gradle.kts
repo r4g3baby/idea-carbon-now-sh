4ee232b559cd3164d353cdf6f41d03fb6561295a
Rui Pereira <me@ruipereira.dev>
2024-04-06 16:12:16 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ fun environment(key: String) = providers.environmentVariable(key)
 plugins {
     id("java") // Java support
     alias(libs.plugins.kotlin) // Kotlin support
-    alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
+    alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
 }
 
 group = properties("pluginGroup").get()
